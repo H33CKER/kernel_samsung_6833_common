6b69ff7f72e0d92cdf9dca180b10eec073a170fb
H33CKER <gnanadan2010@outlook.com>
2025-11-01 20:59:34 +0000

    Overclock big cores to 2.4Ghz (keep little at 2Ghz)

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6833/mtk_cpufreq_opp_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6833/mtk_cpufreq_opp_table.h
@@ -34,7 +34,7 @@
 #define CPU_DVFS_FREQ15_LL_FY		500000		/* KHz */
 
 /* for DVFS OPP table B */
-#define CPU_DVFS_FREQ0_L_FY		2203000		/* KHz */
+#define CPU_DVFS_FREQ0_L_FY		2400000		/* KHz */
 #define CPU_DVFS_FREQ1_L_FY		2087000		/* KHz */
 #define CPU_DVFS_FREQ2_L_FY		1995000		/* KHz */
 #define CPU_DVFS_FREQ3_L_FY		1903000		/* KHz */
